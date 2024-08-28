8c1aa871f073cf54a1ea4627b61cd318c54da8f5
Thandolwethu <thandwem045@gmail.com>
2024-08-28 15:54:12 +0200

    Debugged JavaScript for challenge D

--- challenge-d.js
+++ challenge-d.js
@@ -1,5 +1,6 @@
-import company form 'configuration'
-import year form 'configuration'
+import {company} from './config.js';    //Changed form to from and added curly braces
+import {year} from './config.js';
 
-const message = '© ' + company + ' (' + year + ')'
+const message = '© ' + company + ' (' + year + ')' // Removed brackets
 document.querySelector('footer').innerText = message
+
